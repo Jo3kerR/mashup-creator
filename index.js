3f0242ab9dc296db5d0c73aba60b2cad55f2344a
Abhinab Saha <53913877+Jo3kerR@users.noreply.github.com>
2022-12-16 14:47:12 +0530

    Dev (#2)
    * refactoring
    
    * refactoring

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ const { updateAllProblems } = require("./src/UpdateProblemset");
 
 require("dotenv/config");
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
-app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
@@ -20,9 +16,8 @@ app.use(cors());
 app.use("/create", require("./src/AppRouter"));
 app.use("/contests", require("./src/ContestRouter"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+// cron job to update problem set from codeforces API every 24 hours
+cron.schedule("0 0 * * *", updateAllProblems);
 
 // cron job to update problem set from codeforces API every 24 hours
 cron.schedule("0 0 * * *", updateAllProblems);
